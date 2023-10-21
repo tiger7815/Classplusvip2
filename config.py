e8ab5f6e107d4c39217383f024f7ffbeebbd4afb
pjvip <140431425+pjvip@users.noreply.github.com>
2023-10-21 23:50:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,15 +11,6 @@ class Config(object):
     PASS_DB = int(os.environ.get("PASS_DB", "721")
     OWNER = int(os.environ.get("OWNER", ""))
     LOG = -1001982419675
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
-        ADMINS.append(int(x))
-try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
-        ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
-ADMINS.append(OWNER)
+
 
 
