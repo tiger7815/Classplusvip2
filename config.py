cadd151335aa6f2988acf43bd097af201819eecc
pjvip <140431425+pjvip@users.noreply.github.com>
2023-10-21 23:13:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
 import os
 
-API_ID = int(os.environ["API_ID", ""]
-
-API_HASH = os.environ.get("API_HASH", "")
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
-PASS_DB = int(os.environ.get("PASS_DB", "721"))
-
-OWNER = int(os.environ.get("OWNER", ""))
-
-LOG = -1001982419675
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    API_ID = int(os.environ["API_ID", ""]
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    PASS_DB = int(os.environ.get("PASS_DB", "721")
+    OWNER = int(os.environ.get("OWNER", ""))
+    LOG = -1001982419675
 
 try:
     ADMINS=[]
