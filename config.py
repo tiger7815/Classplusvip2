4127367467e7cd89e471a0f9476e905a90e88457
pjvip <140431425+pjvip@users.noreply.github.com>
2023-10-21 23:44:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,6 @@
 import os
-API_ID = int(os.environ["API_ID", ""]
 
-API_HASH = os.environ.get("API_HASH", "")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
-PASS_DB = int(os.environ.get("PASS_DB", "721"))
-
-OWNER = int(os.environ.get("OWNER", ""))
-
-LOG = -1001982419675
-
-try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
-        ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
-ADMINS.append(OWNER)
 class Config(object):
     # get a token from @BotFather
     pass
